86fd770bf790cc2b0c906f44c2b29bf34c1cb9f1
Mikabrytu <mikabrytu@gmail.com>
2020-07-06 06:40:07 -0300

    Adding base plugin code with example

---
+++ Mucha Image Loader/Assets/Scripts/Example/ListController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mikabrytu.Mucha.Example
+{
+    public class ListController : MonoBehaviour
+    {
+        private string[] images = {
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png",
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png",
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png",
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png"
+        };
+
+        [SerializeField] private GameObject imageItem;
+        [SerializeField] private Transform content;
+
+        private Mucha mucha;
+
+        private void Start()
+        {
+            mucha = new Mucha();
+
+            foreach (string image in images)
+            {
+                GameObject clone = Instantiate(imageItem, content);
+                mucha.load(image).into(clone.GetComponent<RawImage>()).start();
+            }
+        }
+    }
+}
---
+++ Mucha Image Loader/Assets/Scripts/Mucha.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace Mikabrytu.Mucha
+{
+    public class Mucha
+    {
+        private const string TAG = "#DOWNLOAD ASYNC#";
+
+        private RawImage image;
+        private string url;
+
+        public Mucha load(string url)
+        {
+            this.url = url;
+            return this;
+        }
+
+        public Mucha into(RawImage image)
+        {
+            this.image = image;
+            return this;
+        }
+
+        public async void start()
+        {
+            image.texture = await Download(url);
+        }
+
+        private async Task<Texture2D> Download(string url)
+        {
+            using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(url))
+            {
+                var asyncOp = request.SendWebRequest();
+
+                while (!asyncOp.isDone)
+                    await Task.Delay(1000 / 30);
+
+                if (request.isNetworkError || request.isHttpError)
+                {
+                    Debug.LogError($"{TAG} network error: {request.error} / {request.url}");
+                    return null;
+                }
+                else
+                {
+                    Debug.Log($"{TAG} thumb downloaded!");
+                    return DownloadHandlerTexture.GetContent(request);
+                }
+            }
+        }
+    }
+}
